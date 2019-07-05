1fb0f102348584fcddf05c2d897aedd7149a11c6
litao01 <litao01@360.net>
2019-07-05 10:47:21 +0800

    连接 mongodb 数据库

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://fxyf1991:19910902LItao@cluster0-b1u3a.mongodb.net/test?retryWrites=true&w=majority',
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db)
+  .then((result) => {
+    console.log('connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.get('/', (req, res) => {
+  res.send('welcome');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
