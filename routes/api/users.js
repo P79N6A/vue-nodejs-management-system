dc5f3c41b6d5eca625300fd463ccfe54a04f23bd
litao01 <litao01@360.net>
2019-07-05 16:30:18 +0800

    添加登录接口

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,4 +49,27 @@ router.post('/register', (req, res) => {
     .catch((err) => {});
 });
 
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ email: '用户不存在' });
+      }
+      // 匹配密码
+      bcrypt
+        .compare(password, user.password)
+        .then((isMatch) => {
+          if (isMatch) {
+            res.json({ msg: 'success' });
+          } else {
+            return res.status(400).json({ password: '密码错误' });
+          }
+        })
+        .catch((err) => {});
+    })
+    .catch((err) => {});
+});
+
 module.exports = router;
